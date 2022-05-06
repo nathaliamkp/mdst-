caf3cfe930692be271bddd4368bc1ecb6ecd12d1
zorandir <nah.manso@gmail.com>
2022-05-06 16:11:49 -0300

    criando estrutura do projeto

---
+++ src/main/java/br/com/midst/midst/MidstApplication.java
@@ -0,0 +1,13 @@
+package br.com.midst.midst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/midst/midst/MidstApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.midst.midst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
